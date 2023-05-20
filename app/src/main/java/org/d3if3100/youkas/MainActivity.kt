4bcb9c69a6b863db2841c5fdcd96c29ef595a1ad
risazhrn <risazahrani@student.telkomuniversity.ac.id>
2023-05-20 09:13:35 +0700

    Tambah View Binding

--- app/src/main/java/org/d3if3100/youkas/MainActivity.kt
+++ app/src/main/java/org/d3if3100/youkas/MainActivity.kt
@@ -2,10 +2,15 @@ package org.d3if3100.youkas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if3100.youkas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
